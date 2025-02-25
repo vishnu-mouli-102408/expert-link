e0944dfa8be6228e0b65bda513d3a7b846d9d85c
Vishnu Mouli <vishnumouli0@gmail.com>
2025-02-25 22:41:58 +0530

    build: added prettier setup + tailwindcss plugin + sort imports plugin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,60 @@
-import type { Metadata } from "next"
-import { Providers } from "./components/providers"
+import type { Metadata } from "next";
+import { Providers } from "./components/providers";
+import { Open_Sans, Poppins, Roboto } from "next/font/google";
 
-import "./globals.css"
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import { Suspense } from "react";
+import { LoadingSpinner } from "./components/Spinner";
 
 export const metadata: Metadata = {
-  title: "JStack App",
-  description: "Created using JStack",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
-}
+	title: "Expert Link",
+	description:
+		"A platform where users can book 1-on-1 video/audio calls with industry experts (doctors, lawyers, financial advisors, startup mentors, etc.).",
+	icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+const openSans = Open_Sans({
+	subsets: ["latin"],
+	variable: "--font-open-sans",
+	weight: ["300", "400", "500", "600", "700"],
+	display: "swap",
+	preload: true,
+});
+
+const roboto = Roboto({
+	subsets: ["latin"],
+	variable: "--font-roboto",
+	weight: ["300", "400", "500", "700"],
+	display: "swap",
+	preload: true,
+});
+const poppins = Poppins({
+	subsets: ["latin"],
+	variable: "--font-poppins",
+	weight: ["300", "400", "500", "600", "700", "800"],
+	display: "swap",
+	preload: true,
+});
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className="antialiased">
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  )
+	return (
+		<html
+			suppressHydrationWarning
+			lang="en"
+			className={cn(poppins.variable, openSans.variable, roboto.variable, "antialiased dark")}
+		>
+			<body className="antialiased">
+				<Suspense fallback={<LoadingSpinner mainClassName="h-screen" />}>
+					<main className="flex flex-col flex-1 relative">
+						<Providers>{children}</Providers>
+					</main>
+				</Suspense>
+			</body>
+		</html>
+	);
 }
