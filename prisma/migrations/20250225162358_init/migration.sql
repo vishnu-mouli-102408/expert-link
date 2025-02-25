7145b6e98b3fba27fea22464a90a01d4a8b8add3
Vishnu Mouli <vishnumouli0@gmail.com>
2025-02-25 22:24:15 +0530

    init

---
+++ prisma/migrations/20250225162358_init/migration.sql
@@ -0,0 +1,34 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN', 'EXPERT');
+
+-- CreateEnum
+CREATE TYPE "BookingStatus" AS ENUM ('PENDING', 'CONFIRMED', 'COMPLETED', 'CANCELLED');
+
+-- CreateEnum
+CREATE TYPE "PaymentStatus" AS ENUM ('PENDING', 'PAID', 'FAILED');
+
+-- CreateEnum
+CREATE TYPE "CallType" AS ENUM ('AUDIO', 'VIDEO');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "phone" TEXT,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "profilePicture" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "gender" TEXT,
+    "bio" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
---
+++ src/db.ts
@@ -0,0 +1,32 @@
+import { neonConfig, Pool } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@prisma/client";
+import ws from "ws";
+
+declare global {
+	// eslint-disable-next-line no-var
+	var cachedPrisma: PrismaClient;
+}
+
+// Configure Neon to use WebSockets
+neonConfig.webSocketConstructor = ws;
+
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+	const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+	const adapter = new PrismaNeon(pool);
+	prisma = new PrismaClient({ adapter });
+} else {
+	if (!global.cachedPrisma) {
+		const pool = new Pool({
+			connectionString: process.env.DATABASE_URL,
+			max: 10,
+			idleTimeoutMillis: 30000,
+		});
+		const adapter = new PrismaNeon(pool);
+		global.cachedPrisma = new PrismaClient({ adapter });
+	}
+	prisma = global.cachedPrisma;
+}
+
+export const db = prisma;
