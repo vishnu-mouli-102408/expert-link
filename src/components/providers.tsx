d49fa46069ae6573bf9b6ba16ca32e9a0a709f9d
Vishnu Mouli <vishnumouli0@gmail.com>
2025-02-26 23:04:38 +0530

    updated query client

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,26 +1,12 @@
 "use client";
 
 import { PropsWithChildren, useState } from "react";
-import {
-  QueryCache,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
-import { HTTPException } from "hono/http-exception";
+import { QueryClientProvider } from "@tanstack/react-query";
+
+import { getQueryClient } from "@/lib/get-query-client";
 
 export const Providers = ({ children }: PropsWithChildren) => {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        queryCache: new QueryCache({
-          onError: (err) => {
-            if (err instanceof HTTPException) {
-              // global error handling, e.g. toast notification ...
-            }
-          },
-        }),
-      })
-  );
+  const [queryClient] = useState(() => getQueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
