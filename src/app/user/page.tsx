28cf81acf8c18b34a9949b4826423e09529b5668
Vishnu Mouli <vishnumouli0@gmail.com>
2025-03-08 12:45:20 +0530

    updated package.json

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -2,7 +2,7 @@ import { UserOverview } from "@/components";
 
 function Page() {
   return (
-    <div className="container mx-auto p-6">
+    <div className="container bg-black/50 mx-auto p-6">
       <UserOverview />
       {/* <div
         className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-12"
