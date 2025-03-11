24dd7afee7a5a9153eddb268cbe7803165e7ca98
Vishnu Mouli <vishnumouli0@gmail.com>
2025-03-12 00:44:49 +0530

    generate static params for profiles

--- src/app/user/expert-profile/[expertId]/page.tsx
+++ src/app/user/expert-profile/[expertId]/page.tsx
@@ -1,11 +1,21 @@
 import type { Metadata } from "next";
 import { ExpertProfile } from "@/components";
 
+import { client } from "@/lib/client";
+
 export const metadata: Metadata = {
   title: "Expert Profile",
   description: "This is the layout for the expert profile page.",
   icons: [{ rel: "icon", url: "/favicon/favicon.ico" }],
 };
+
+export async function generateStaticParams() {
+  const response = await client.user.getAllExperts.$get();
+  const expertProfiles = await response.json();
+  return expertProfiles?.data?.experts?.map((expertProfile) => ({
+    expertId: expertProfile.id,
+  }));
+}
 
 const Page = async ({ params }: { params: Promise<{ expertId: string }> }) => {
   const { expertId } = await params;
